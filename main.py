caf0eacd3832f74e0650e635aad5ca6ccce29e8b
Mannaward <88430213+Mannaward25@users.noreply.github.com>
2023-01-11 15:05:30 +0300

    Added new print

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+numbers = [number for number in range(0,30)]
 for number in numbers:
     if number % 3 == 0:
         print("Число", number, "делится на 3")
