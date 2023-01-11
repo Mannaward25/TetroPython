754c4fd232e227be57b28138d21e5a437e7c6e90
Mannaward <88430213+Mannaward25@users.noreply.github.com>
2023-01-11 15:19:30 +0300

    Added new print

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-numbers = [number for number in range(0,30)]
-for number in numbers:
-    if number % 3 == 0:
-        print("Число", number, "делится на 3")
-    else:
-        print("Число", number, "не делится на 3")
+numbers = [number for number in range(0, 30) if number % 3 != 0]
+print(numbers)
