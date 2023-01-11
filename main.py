97cce9f92434cebe8043b014589bacc4c5406c61
Mannaward <88430213+Mannaward25@users.noreply.github.com>
2023-01-11 15:04:38 +0300

    Added new print

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [2, 4, 6, 8, 10]
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 for number in numbers:
-    if number % 2 == 1:
-        print(number)
-        break
+    if number % 3 == 0:
+        print("Число", number, "делится на 3")
     else:
-        print("Иди нахуй")
+        print("Число", number, "не делится на 3")
