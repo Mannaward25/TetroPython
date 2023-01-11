c2d65538d712f40b94963b47c03099803e3a2596
Mannaward <88430213+Mannaward25@users.noreply.github.com>
2023-01-11 14:44:41 +0300

    Added new print

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Molodec")
-print("Biggy")
+numbers = [2, 4, 6, 8, 10]
+for number in numbers:
+    if number % 2 == 1:
+        print(number)
+        break
+    else:
+        print("Иди нахуй")
